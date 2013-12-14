084df85f32cb7a0ead48a7eaf0fe321594c09339
Kovacs Arthur <kovacs.arthur.kraid@gmail.com>
2013-12-14 02:29:47 +0200

    Initial Push

---
+++ src/app/app.js
@@ -0,0 +1,27 @@
+angular.module( 'ngBoilerplate', [
+  'templates-app',
+  'templates-common',
+  'ngBoilerplate.home',
+  'ngBoilerplate.about',
+  'ui.router.state',
+  'ui.route'
+])
+
+.config( function myAppConfig ( $stateProvider, $urlRouterProvider, $locationProvider ) {
+    $locationProvider.html5Mode(true).hashPrefix('!');
+    $urlRouterProvider.otherwise( '/home' );
+})
+
+.run( function run () {
+})
+
+.controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
+  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+    if ( angular.isDefined( toState.data.pageTitle ) ) {
+      $scope.pageTitle = toState.data.pageTitle + ' | ngBoilerplate' ;
+    }
+  });
+})
+
+;
+
